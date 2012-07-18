7912800243925f0fb324d5b1af5d1c4844228808
Dan <Dan@Nitr0SS>
2012-07-19 00:19:49 +0100

    Debug Code

--- oregen/src/com/nitr0uk/www/oregen/OreGen.java
+++ oregen/src/com/nitr0uk/www/oregen/OreGen.java
@@ -1,5 +1,53 @@
 package com.nitr0uk.www.oregen;
 
-public class OreGen {
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class OreGen extends JavaPlugin{
+
+	private static Configuration configuration = null;
+
+	@Override
+	public void onEnable()
+	{
+		if (configuration != null)
+		{
+			//something went horribly, horribly wrong
+			throw new InternalError();
+		}
+		configuration = getConfig();
+		getCommand("oregen").setExecutor(this);
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		configuration = null;
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		System.out.println("Command Executed!");
+		World world = getServer().getWorld("world");
+		Chunk chunk = world.getChunkAt(0,0);
+		System.out.println(chunk.getX());
+		System.out.println(chunk.getZ());
+		chunk = world.getChunkAt(0,1);
+		System.out.println(chunk.getX());
+		System.out.println(chunk.getZ());
+		Block block = chunk.getBlock(22, 0, 0);
+		System.out.println("Block ID: " + block.getTypeId());
+		return true;
+	}
+	
+	public static Configuration getConfiguration() {
+		return configuration;
+	}
 }
+
